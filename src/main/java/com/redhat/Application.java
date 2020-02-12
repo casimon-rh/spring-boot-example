21df4b912fad93278e1efbf7a8517e7665133973
Carlos Eduarco Simón Zepeda <casimon@redhat.com>
2020-02-12 11:27:19 -0600

    logs

--- src/main/java/com/redhat/Application.java
+++ src/main/java/com/redhat/Application.java
@@ -1,13 +1,17 @@
 package com.redhat;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
 public class Application {
+
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
     public static void main(String[] args) {
-        System.out.println("Hello word");
         SpringApplication.run(Application.class, args);
+        logger.info("Hello World App Started");
     }
 }
--- src/main/java/com/redhat/controller/Greeting.java
+++ src/main/java/com/redhat/controller/Greeting.java
@@ -1,13 +1,19 @@
 package com.redhat.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
 public class Greeting {
+
+  private final Logger logger = LoggerFactory.getLogger(Greeting.class);
+
   @GetMapping(value="/")
   public String getMethodName() {
-      return "Hello World";
+    logger.info(">>> Hello world requested <<<");
+    return "Hello World";
   }
 }
